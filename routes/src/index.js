0b5797940b31056afbaf0fc51b7c9be8bb92fe84
Daniel <danieltellezc@gmail.com>
2021-11-29 19:51:04 -0600

    Carpetas ordenadas

--- routes/src/index.js
+++ routes/src/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 
-require('./database')
-app.use(require('../routes/index'))
+require('./database');
+app.use(require('../index'));
 
 app.listen(3000);
 console.log("Este es el puerto: ", 3000);
