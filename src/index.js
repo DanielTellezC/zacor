95a307b2d02bb3faa95c2a8a9546a5ac150c5897
Daniel <danieltellezc@gmail.com>
2021-11-29 19:54:44 -0600

    Carpetas ordenadas de manera corracta

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 require('./database');
-app.use(require('../index'));
+app.use(require('./routes/index'));
 
 app.listen(3000);
 console.log("Este es el puerto: ", 3000);
