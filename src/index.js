fb3e0ec31a70433c5b80cfad431f691d04b7178a
Daniel <danieltellezc@gmail.com>
2021-12-02 17:16:39 -0600

    autenticacion de cuentas, signin y signup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,41 @@
 const express = require("express");
+const engine = require('ejs-mate');
+const path = require('path');
+const morgan = require('morgan');
+const passport = require('passport');
+const session = require('express-session');
+const flash = require('connect-flash');
+
 const app = express();
-
+require('./passport/local-auth');
 require('./database');
-app.use(require('./routes/index'));
 
+// Configuraciones
+app.set('views', path.join(__dirname,'views'));
+app.engine('ejs', engine);
+app.set('view engine','ejs');
 app.listen(3000);
+
+//Midlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(session({
+    secret: 'mysecretsession',
+    resave: true,
+    saveUninitialized: false
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req, res, next) => {
+    app.locals.signupMessage = req.flash('signupMessage');
+    next();
+})
+//Routes
+app.use('/',require('./routes/index'));
+
+
+
+//inicio del servidor
 console.log("Este es el puerto: ", 3000);
